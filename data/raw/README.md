df9531160850af758923e70721d32ef90eb0e240
GiacomoFilippin <giacomo.filippin12@gmail.com>
2025-04-04 21:09:54 +0200

    still not interesting

--- data/raw/README.md
+++ data/raw/README.md
@@ -1,1 +1,18 @@
-Placholder for describing raw data
+fma_medium.zip contains raw, 30s mp3 audio tracks.
+the idea is to process the various tracks without decompressing the dataset
+this way i can extract features without dimensionality issues,
+and i can just save each 30s mp3's spectrogram, alongside its targets (yet to be decided)
+
+fma_metadata.zip contains CSVs about features, such as:
+'fma_metadata/checksums', 
+'fma_metadata/not_found.pickle', 
+'fma_metadata/raw_genres.csv', 
+'fma_metadata/raw_albums.csv', 
+'fma_metadata/raw_artists.csv', 
+'fma_metadata/raw_tracks.csv', 
+'fma_metadata/tracks.csv', 
+'fma_metadata/genres.csv', 
+'fma_metadata/raw_echonest.csv', 
+'fma_metadata/echonest.csv', 
+'fma_metadata/features.csv'
+further exploration of which could be the best tergets to test multitask learning has to be made.
--- notebook/first_testing.py
+++ notebook/first_testing.py
@@ -1,16 +1,31 @@
+#%%
 import zipfile
 import io
+import os 
+import sys
+# Aggiungi la root del progetto al Python path
+project_root = os.path.dirname(os.path.abspath(''))
+sys.path.append(project_root)  # Ora Python trova i moduli nella root
 
-# Open the ZIP file
-with zipfile.ZipFile('dataset.zip', 'r') as zip_ref:
+#%% explore mp3s zip
+zip_path = os.path.join('..', 'data', 'raw', 'fma_medium.zip')
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     # List all files in the archive
     print(zip_ref.namelist())
-    
     # Read a specific file (e.g., 'data/track1.mp3') as bytes
-    with zip_ref.open('data/track1.mp3') as file_in_zip:
+    with zip_ref.open('fma_medium/README.txt') as file_in_zip:
         audio_bytes = file_in_zip.read()
-    
+"""    
     # Process the file (e.g., convert to spectrogram)
     import librosa
     y, sr = librosa.load(io.BytesIO(audio_bytes), sr=44100)
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)"""
+# %%
+print(audio_bytes)
+# %% explore metadata zips
+zip_path = os.path.join('..', 'data', 'raw', 'fma_metadata.zip')
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    # List all files in the archive
+    print(zip_ref.namelist())
+
+# %%
