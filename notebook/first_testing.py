58a6a82f5c222bf4354ae987f0505c017bfb135b
GiacomoFilippin <giacomo.filippin12@gmail.com>
2025-04-09 20:17:40 +0200

    first explorations

--- notebook/first_testing.py
+++ notebook/first_testing.py
@@ -3,12 +3,25 @@ import zipfile
 import io
 import os 
 import sys
+import numpy as np
 # Aggiungi la root del progetto al Python path
 project_root = os.path.dirname(os.path.abspath(''))
 sys.path.append(project_root)  # Ora Python trova i moduli nella root
 from scripts.processing_functions import load_features
 # Load metadata and features.
 tracks, genres, features, echonest = load_features()
+print('Echonest features available for {} tracks.'.format(len(echonest)))
+
+features_clean = features.iloc[3:]
+np.testing.assert_array_equal(features_clean.index, tracks.index)
+assert echonest.index.isin(tracks.index).all()
+
+tracks_medium = tracks[tracks['set', 'subset'] <= 'medium']
+
+tracks_medium.shape, features_clean.shape, echonest.shape
+
+durations = tracks_medium['track', 'duration']
+genres = tracks_medium['track', 'genre_top']
 #%% explore mp3s zip
 zip_path = os.path.join('..', 'data', 'raw', 'fma_medium.zip')
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
